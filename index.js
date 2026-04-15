b1a45d8f55192e881d94356e0a4d574c5c5becf2
EduardXanderM <exmmariquita@gmail.com>
2026-04-15 14:00:13 +0400

    Added Navbar and Header website responsiveness for mobile and tablet devices. Also added a menu toggle button for mobile navigation.

---
+++ index.js
@@ -0,0 +1,25 @@
+// Navbar scroll hide/show
+
+let lastScrollTop = 0;
+const navbar = document.querySelector("nav");
+
+window.addEventListener("scroll", function () {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScroll > lastScrollTop) {
+  navbar.style.transform = "translateY(-100%)";
+} else {
+  navbar.style.transform = "translateY(0)";
+}
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
+
+// Hamburger menu toggle
+
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
